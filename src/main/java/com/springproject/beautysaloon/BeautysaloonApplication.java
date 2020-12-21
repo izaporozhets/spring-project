da1903573eaa8f99657e73cc4911e1aa69fe74ae
izaporozhets <49363845+izaporozhets@users.noreply.github.com>
2020-12-21 17:33:32 +0200

    heroku

--- src/main/java/com/springproject/beautysaloon/BeautysaloonApplication.java
+++ src/main/java/com/springproject/beautysaloon/BeautysaloonApplication.java
@@ -1,18 +1,11 @@
 package com.springproject.beautysaloon;
 
-import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Set;
-import java.util.TreeSet;
 
 @SpringBootApplication
 public class BeautysaloonApplication {
     public static void main(String[] args) {
-
-
         SpringApplication.run(BeautysaloonApplication.class, args);
     }
 
